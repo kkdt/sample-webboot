a463bb8c7e213de25eb545d5d7681077dbf1bf2e
thinh ho <me@kkdt.one>
2017-07-09 01:10:01 -0400

    Gradle Application/Distribution Plugin - Handling multiple start scripts in one baseline - Distribution ignores the root directory that is the project name

---
+++ src/main/java/kkdt/sample/webboot/CurrentTime.java
@@ -0,0 +1,16 @@
+/** 
+ * Copyright (C) 2017 thinh ho
+ * This file is part of 'sample-webboot' which is released under the MIT license.
+ * See LICENSE at the project root directory.
+ */
+package kkdt.sample.webboot;
+
+import java.util.Date;
+
+public class CurrentTime {
+
+   public static void main(String[] args) {
+      System.out.println(new Date());
+   }
+
+}
---
+++ src/main/java/kkdt/sample/webboot/HelloWorld.java
@@ -0,0 +1,15 @@
+/** 
+ * Copyright (C) 2017 thinh ho
+ * This file is part of 'sample-webboot' which is released under the MIT license.
+ * See LICENSE at the project root directory.
+ */
+package kkdt.sample.webboot;
+
+public class HelloWorld {
+
+   public static void main(String[] args) {
+      System.out.println("HELLO WORLD!");
+
+   }
+
+}
